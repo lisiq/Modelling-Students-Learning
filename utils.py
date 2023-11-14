ca00d862ffbee7e411e9bde952ccb60d2d25c2a3
Benjamin Garzon Jimenez de Cisneros <benjamin.garzon@ife.uzh.ch>
2023-11-14 15:45:58 +0000

    Reincluded AUC

--- utils.py
+++ utils.py
@@ -101,7 +101,7 @@ def calculate_metrics(y_true, pred):
     y_predsoft = pred.squeeze().numpy()#softmax(pred).numpy()[:, 1]
     y_pred = pred.squeeze().round().long().numpy()#.argmax(dim=1, keepdim=True).view(-1).numpy()
     return {
-            # 'AUC':roc_auc_score(y_true, y_predsoft), #
+            'AUC':roc_auc_score(y_true, y_predsoft), #
             # 'F1-score-weighted':f1_score(y_true, y_pred, average='weighted'), #
             # 'F1-score-macro':f1_score(y_true, y_pred, average='macro'),
             # 'F1-score-micro':f1_score(y_true, y_pred, average='micro'),
