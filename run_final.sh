b2a25b62a35163f6967203e422f75fa40d7c1617
Benjamin Garzon Jimenez de Cisneros <benjamin.garzon@ife.uzh.ch>
2024-01-10 11:16:02 +0000

    item features model

--- run_final.sh
+++ run_final.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 papermill -p IRT_DIMS 1 -p DATASET full GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_IRT_full.html > logs/GNN_batch_IRT_full.log 
 papermill -p IRT_DIMS 0 -p DATASET full GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_full.html > logs/GNN_batch_SAGE_full.log
+papermill -p IRT_DIMS 0 -p DATASET full -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_full_scales.html > logs/GNN_batch_SAGE_full_scales.log
 papermill -p IRT_DIMS 0 -p DATASET matrix GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_matrix.html > logs/GNN_batch_SAGE_matrix.log
 papermill -p IRT_DIMS 0 -p DATASET topic GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_topic.html > logs/GNN_batch_SAGE_topic.log
 ./run_plots.sh
