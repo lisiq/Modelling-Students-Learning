48f693a92ca27c23f259939aa9e27d21a5a50ea4
Benjamin Garzon Jimenez de Cisneros <benjamin.garzon@ife.uzh.ch>
2024-01-15 14:30:31 +0000

    fixed item_features

--- run_final.sh
+++ run_final.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 papermill -p IRT_DIMS 1 -p DATASET full GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_IRT_full.html > logs/GNN_batch_IRT_full.log 
 papermill -p IRT_DIMS 0 -p DATASET full GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_full.html > logs/GNN_batch_SAGE_full.log
-papermill -p IRT_DIMS 0 -p DATASET matrix -p ITEM_FEATURES GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_matrix_scales.html > logs/GNN_batch_SAGE_matrix.log
-papermill -p IRT_DIMS 0 -p DATASET topic -p ITEM_FEATURES GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_topic_scales.html > logs/GNN_batch_SAGE_topic.log
+papermill -p IRT_DIMS 0 -p DATASET matrix -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_matrix_scales.html > logs/GNN_batch_SAGE_matrix.log
+papermill -p IRT_DIMS 0 -p DATASET topic -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_topic_scales.html > logs/GNN_batch_SAGE_topic.log
 
 papermill -p IRT_DIMS 0 -p DATASET full -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_full_scales.html > logs/GNN_batch_SAGE_full_scales.log
 
