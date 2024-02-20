51a2b1c8edab463d73956a8adf3af20a2d66b9ac
Benjamin Garzon Jimenez de Cisneros <benjamin.garzon@ife.uzh.ch>
2024-02-20 16:50:31 +0000

    run tests

--- run_temporal_tests.sh
+++ run_temporal_tests.sh
@@ -3,4 +3,9 @@
 ## matrix and topic
 # with scales
 papermill -p IRT_DIMS 0 -p DATASET matrix -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_matrix_scales.html > logs/GNN_batch_SAGE_matrix_scales.log
-papermill -p IRT_DIMS 1 -p DATASET matrix -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_IRT_matrix_scales.html > logs/GNN_batch_IRT_matrix_scales.log 
+#papermill -p IRT_DIMS 1 -p DATASET matrix -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_IRT_matrix_scales.html > logs/GNN_batch_IRT_matrix_scales.log 
+
+## full
+papermill -p IRT_DIMS 0 -p DATASET full -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_SAGE_full_scales.html > logs/GNN_batch_SAGE_full_scales.log
+#papermill -p IRT_DIMS 1 -p DATASET full -p ITEM_FEATURES True GNN_final.ipynb | jupyter-nbconvert --stdin --no-input --to html --output vis/GNN_batch_IRT_full_scales.html > logs/GNN_batch_IRT_full_scales.log 
+./run_plots.sh
